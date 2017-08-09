3938cbe5fffdc72fad686add1959f8a9e0801f95
Laurentius Purba <lrpurba@yahoo.com>
2017-08-08 21:14:45 -0400

    Initial commit of rock, paper, scissors, lizard, spock.

---
+++ rpsls.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2.7
+
+# Rock-paper-scissors-lizard-Spock
+
+'''
+The key idea of this program is to equate the strings
+"rock", "paper", "scissors", "lizard", "Spock" to numbers as follows:
+
+0 - rock
+1 - Spock
+2 - paper
+3 - lizard
+4 - scissors
+'''
