4eb2ca05cb5f054b6cf454d3dcb3b95fb7e36246
Laurentius Purba <lrpurba@yahoo.com>
2017-08-08 20:23:11 -0400

    Update readme with code skulptor url, and initial blackjack.py commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # python-coursera
+
+In order to get the right result, make sure to run any codes in this repository
+in [CodeSkulptor](http://www.codeskulptor.org/)
---
+++ blackjack.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python2.7
+
+import simplegui
+import random
+
+# Mini-project #6 - Blackjack
+
+# load card sprite - 936x384 - source: jfitz.com
+CARD_SIZE = (72, 96)
+CARD_CENTER = (36, 48)
+card_images = simplegui.load_image("http://storage.googleapis.com/codeskulptor-assets/cards_jfitz.png")
+
+CARD_BACK_SIZE = (72, 96)
+CARD_BACK_CENTER = (36, 48)
+card_back = simplegui.load_image("http://storage.googleapis.com/codeskulptor-assets/card_jfitz_back.png")
+
+# intialize some useful global variables
+in_play = False
+outcome = ""
+score = 0
+
+# define globals for cards
+SUITS = ('C', 'S', 'H', 'D')
+RANKS = ('A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K')
+VALUES = {'A':1, '2':2, '3':3, '4':4, '5': 5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':10, 'Q':10, 'K':10}
+
+# define card class
+class Card:
+    def __init__(self, suit, rank):
+        if (suit in SUITS) and (rank in RANKS):
+            self.suit = suit
+            self.rank = rank
+        else:
+            self.suit = None
+            self.rank = None
+            print "Invalid card: ", suit, rank
+
+    def __str__(self):
+        return self.suit + self.rank
+
+    def get_suit(self):
+        return self.suit
+
+    def get_rank(self):
+        return self.rank
+
+    def draw(self, canvas, pos):
+        pass
